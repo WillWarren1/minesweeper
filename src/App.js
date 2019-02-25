181f183c5fe1126a20b29b8b78e7464e45c3e8c3
Will Warren <willwarrenmail@gmail.com>
2019-02-25 15:47:24 -0500

    incorporating axios and api compatibility

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
+import axios from 'axios'
+
+// return (
+//   <>
+//    <table>
+//      <tbody>
+//        {this.state.game.map(row => {
+//           return (
+//               <tr>
+//                  {row.map(col => {
+//                    return <td>{col}</td>
+//                  })}
+//               </tr>
+//              )
+//            })}
+//       <tbody>
+//    <table>
+//   </>
 
 class App extends Component {
+  state = {
+    game: [[], [], [], [], [], [], [], []]
+  }
+  componentDidMount() {
+    axios
+      .post('https://minesweeper-api.herokuapp.com/games', {
+        difficulty: 0
+      })
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
+
   render() {
     return (
       <>
