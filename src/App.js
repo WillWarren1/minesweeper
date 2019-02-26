af894170ed32644fa11fe568f925eae81f5423c8
Will Warren <willwarrenmail@gmail.com>
2019-02-26 13:43:16 -0500

    can left click tiles to console log info

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ import axios from 'axios'
 
 class App extends Component {
   state = {
-    game: [[], [], [], [], [], [], [], []]
+    game: [[], [], [], [], [], [], [], []],
+    id: ''
   }
   componentDidMount() {
     axios
@@ -31,8 +32,23 @@ class App extends Component {
       .then(resp => {
         console.log({ resp })
         this.setState({
-          game: resp.data.board
+          game: resp.data.board,
+          id: resp.data.id
         })
+      })
+  }
+  checkTile(x, y) {
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`,
+        {
+          id: this.state.id,
+          row: x,
+          col: y
+        }
+      )
+      .then(resp => {
+        console.log({ resp })
       })
   }
 
@@ -40,20 +56,27 @@ class App extends Component {
     return (
       <>
         <main>
-          <section className="head">this is a header</section>
-          <table>
-            <tbody>
-              {this.state.game.map(row => {
-                return (
-                  <tr>
-                    {row.map(col => {
-                      return <td>{col}</td>
-                    })}
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
+          <figure>
+            <section className="head">this is a header</section>
+            <table>
+              <tbody>
+                {this.state.game.map((row, x) => {
+                  return (
+                    <tr key={x}>
+                      {row.map((col, y) => {
+                        return (
+                          <td key={y} onClick={() => this.checkTile(x, y)}>
+                            {col}
+                            {x},{y}
+                          </td>
+                        )
+                      })}
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </figure>
         </main>
       </>
     )
