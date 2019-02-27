4c2961516ed4fb4328728b07e5e57bb07f8d8584
Will Warren <willwarrenmail@gmail.com>
2019-02-27 13:54:28 -0500

    added styling

--- src/components/cell.js
+++ src/components/cell.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react'
 
 class Cell extends Component {
   render() {
+    let classStyle = 'hidden'
+    let picture = this.props.content
+    switch (picture) {
+      case ' ':
+        classStyle = 'hidden'
+        break
+      case '*':
+        picture = '💣'
+        classStyle = 'revealed-class'
+        break
+      case 'F':
+        picture = '🚩'
+        classStyle = 'hidden'
+        break
+      case '_':
+        picture = ''
+        classStyle = 'revealed-class'
+        break
+      case '@':
+        picture = '🎉'
+        classStyle = 'revealed-class'
+        break
+      default:
+        picture = this.props.content
+        classStyle = 'revealed-class'
+    }
     if (this.props.content !== 'F') {
       return (
         <td
+          className={classStyle}
           onClick={() =>
             this.props.checkTile(this.props.rowIndex, this.props.columnIndex)
           }
@@ -15,12 +42,13 @@ class Cell extends Component {
               this.props.columnIndex
             )
           }>
-          {this.props.content}
+          {picture}
         </td>
       )
     } else {
       return (
         <td
+          className={classStyle}
           onContextMenu={event =>
             this.props.flagTile(
               event,
@@ -28,7 +56,7 @@ class Cell extends Component {
               this.props.columnIndex
             )
           }>
-          {this.props.content}
+          {picture}
         </td>
       )
     }
